79839f1f158869a231f658557371f366de12a0a1
dimchee <dimchee90@gmail.com>
2022-09-22 16:57:47 +0200

    Fix createIssue

--- .github/scripts/createIssue.sh
+++ .github/scripts/createIssue.sh
@@ -1,6 +1,6 @@
 if [ -z "$(gh issue list -l LaTeX -S $1)" ]
 then
-	gh issue create --title "$1" --body "[Zadate stranice](../blob/main/Src/Pages/$1.tex)" --label LaTeX
+	gh issue create --title "$1" --body "[Zadate stranice](../blob/main/Parts/$1.pdf)" --label LaTeX
     echo "Created Issue $1 :D"
 else
     echo "Issue $1 already exists!" 
