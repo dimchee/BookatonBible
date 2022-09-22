070c0eb1a4a036a174fb0227a5ce9e8f3b768a85
dimchee <dimchee90@gmail.com>
2022-09-22 16:21:48 +0200

    Add splitBook action

--- .github/scripts/splitBook.sh
+++ .github/scripts/splitBook.sh
@@ -2,20 +2,18 @@ Book=$1
 Step=$2
 Pages=$(pdftk "$Book" dump_data | grep "NumberOfPages" | cut -d " " -f2)
 
-mkdir -p Delovi
+mkdir -p Parts
 
 for i in $(seq 0 $(($Pages / $Step)))
 do
 	poc=$(($Step * $i + 1))
-	poc=$(($poc - ($i > 0 ? 1 : 0))) # get pages overlapped
+	poc=$(($poc - ($i > 0 ? 1 : 0))) # make pages overlap
 	end=$(($Step * ($i + 1)))
 	end=$(($end < $Pages ? $end : $Pages)) # shouldn't go after last page
 	name=$(printf '%04d-%04d' $poc $end) 
-	file="Delovi/$name.pdf"
-	echo "Izdvajam " $file
+	file="Parts/$name.pdf"
+	echo "Separating " $file
 	pdftk "$Book" cat $poc-$end output $file
-	echo "Pravim Github issue " $name
-	gh issue create --title "$name" --body "[Stranice](../blob/main/$file)" --label LaTeX
 done
-echo "Gotovo :D"
+echo "Done :D"
 
